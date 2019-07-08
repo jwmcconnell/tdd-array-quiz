b0f04133b6c0c79b315423a2d6405035c052452a
Jackmc <jackmc1122@gmail.com>
2019-07-08 14:01:30 -0700

    update the remaining indices to have 'is it' preceding the word and a question mark following.

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
 const dontAskWhy = require('../lib/index');
 
 describe('don`t ask why', () => {
-  it('removes all words from the list with an upper or lower case y', () => {
+  it('removes all words from the list with an upper or lower case y and adds is it {word}?', () => {
     const filtered = dontAskWhy(['hello', 'Yeti', 'me', 'happy']);
-    expect(filtered).toEqual(['hello', 'me']);
+    expect(filtered).toEqual(['is it hello?', 'is it me?']);
   });
 });
